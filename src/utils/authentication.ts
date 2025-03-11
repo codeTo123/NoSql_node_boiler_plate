3218b34d0474a4922ac76209fb0713ce066b2ba3
codeTo123 <contact2santosh.s@gmail.com>
2025-03-11 23:21:25 +0530

    Modify authentication function

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -9,20 +9,21 @@ export interface AppRequest extends Request {
 export const authentication = async (req: AppRequest, res: Response, next: NextFunction) => {
     try {
         const reqHeader = req.headers['authorization']
-        const token = reqHeader.split(" ")[1]
 
-        if (!token) {
-            res.status(500).json({ message: "Token is required!" })
+        if (!reqHeader) {
+            res.status(401).json({ message: "You are not authorized!" })
+        } else {
+            const token = reqHeader.split(" ")[1]
+            JWT.verify(token, process.env.JWT_SECRET, ((err: any, decode: any) => {
+                if (err) {
+                    res.status(401).json({ error: err.message || err })
+                }
+                req.user = decode;
+                req.userId = decode.id;
+                req.role = decode.role;
+                next()
+            }))
         }
-        JWT.verify(token, process.env.JWT_SECRET, ((err: any, decode: any) => {
-            if (err) {
-                res.status(401).json({ error: err.message || err })
-            }
-            req.user = decode;
-            req.userId = decode.id;
-            req.role = decode.role;
-            next()
-        }))
     } catch (err: any) {
         res.status(500).json({ error: err.message || err })
     }
